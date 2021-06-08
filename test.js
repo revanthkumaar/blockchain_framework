3e89d2073376869006dc11ba7b9a54129c93eb16
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-08 20:12:37 +0530

    dasdasd
    asdasd

--- test.js
+++ test.js
@@ -2,7 +2,10 @@ const Blockchain = require('./blockchain');
 //importing a functionality written inside a file called 'blockchain.js'
 const bitcoin = new Blockchain();
 //creating an instance of the file
+bitcoin.createNewBlock(123,'sadasdasda23edasd23das','asd23dasdc234rasd23dqd')
+bitcoin.createNewBlock(654764,'986986986','9869869')
+bitcoin.createNewBlock(123,'sadasdasda23edasd23das','asd23dasdc234rasd23dqd')
 
-const newBlock = bitcoin.createNewBlock(123,'sadasdasda23edasd23das','asd23dasdc234rasd23dqd')
 
-console.log(newBlock)
+
+console.log(bitcoin)
