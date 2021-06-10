87e72cd51d872d19a51a77297827235f01c1db48
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-10 20:32:46 +0530

    zxczxc
    asdasd

--- api.js
+++ api.js
@@ -4,5 +4,9 @@ const bitcoinApp = express()
 bitcoinApp.get('/', function (req, res) {
   res.send('now you are at a node')
 })
+
+bitcoinApp.get('/home',function(req,res){
+    res.send('this is the home page of my node')
+})
  
 bitcoinApp.listen(3000)
